5cdc648b68ca1eee22e1d89cb2754ebf572caeaa
manuel <oneline1821@gmail.com>
2021-04-20 20:17:57 -0500

    termino de funciones basicas

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -44,6 +44,7 @@ class GeneralController extends Controller
             $newVehiculo = [
                 'anio' => $request->anio,
                 'color' => $request->color,
+                'descripcion' =>  $request->descripcion,
                 'empresas_id' => $request->empresa_id,
                 'modelo_id' => $request->modelo_id,
                 'personas_id'=> $request->personas_id,
@@ -63,9 +64,30 @@ class GeneralController extends Controller
         $response = [ 'status'=> true, 'data' =>  $list];
         return response()->json( $response, 200 );
     }
+    public function listServicios(){
+        $list = DB::table('servicios') ->get();
+        $response = [ 'status'=> true, 'data' =>  $list];
+        return response()->json( $response, 200 );
+    }
     public function listModelos(){
         $list = DB::table('modelo') ->get();
         $response = [ 'status'=> true, 'data' =>  $list];
         return response()->json( $response, 200 );
+    }
+    public function buscarCliente($tipo,$documento){
+        try {
+            if($tipo == '01'){ //Para las facturas
+
+            }else{ //Para las otros documentos
+
+            }
+            $data = DB::table('movil')->insertGetId($newVehiculo);
+            $response = [ 'status'=> true, 'data' =>  $data];
+            $codeResponse = 200;
+        } catch (\fException $e) {
+            $response = [ 'status'=> true, 'mensaje' => substr($e->errorInfo[2], 54), 'code' => $e->getCode()];
+            $codeResponse = 500;
+        }
+        return response()->json( $response, $codeResponse );
     }
 }
